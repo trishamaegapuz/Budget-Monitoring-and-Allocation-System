40a8b05a554067ee5fef2684ccca639a95ce92da
Trisha Gapuz <trishamaebobiles@gmail.com>
2026-09-19 07:56:54 +0800

    Enable SSL for Neon database

--- backend/db.js
+++ backend/db.js
@@ -1,4 +1,5 @@
 const { Pool } = require('pg');
+
 require('dotenv').config();
 
 const pool = new Pool({
@@ -7,6 +8,9 @@ const pool = new Pool({
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
